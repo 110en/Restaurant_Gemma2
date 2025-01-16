4f9e00c6866ca6503aed5c159198402f69ff16f2
Your Name <alinilf14@gmail.com>
2025-01-16 14:10:06 -0500

    test.py now outputs both a restaurant name and menu based on the cuisine inputted by user

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain.chains import sequential
 
 load_dotenv()
 
@@ -10,4 +12,15 @@ llm = ChatGroq(
     temperature = 0.7
 )
 
-print(llm.invoke("I want to open a restaurant for Iranian food. Suggest a fancy name for this.").content)
+#print(llm.invoke("I want to open a restaurant for Iranian food. Suggest a fancy name for this.").content)
+prompt_name = PromptTemplate.from_template("I want to open a restaurant for {cuisine} food. Suggest A SINGLE fancy name for this. Remember, ONE name for a {cuisine} type restaurant")
+chain_name = prompt_name | llm
+name = chain_name.invoke(input = {"cuisine" : input("Cuisine Wanted (Press Enter When Finished): ")})
+print(name.content)
+
+prompt_menu = PromptTemplate.from_template("Suggest some menu items for {rest_name}. Return it as a comma separated list.")
+chain_menu = prompt_menu | llm
+menu = chain_menu.invoke(input = {"rest_name" : name.content})
+print(menu.content)
+
+
